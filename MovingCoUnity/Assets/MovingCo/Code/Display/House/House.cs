448e6f6df283dda57652a4d46ca206da7d27db11
Matt Rix <matt@matt-rix.com>
2014-07-19 13:24:51 -0400

    Basic physics scene set up

--- MovingCoUnity/Assets/MovingCo/Code/Display/House/House.cs
+++ MovingCoUnity/Assets/MovingCo/Code/Display/House/House.cs
@@ -5,17 +5,50 @@ using System.Collections.Generic;
 
 public class House : FContainer
 {
-	public FSprite houseOutline;
+	public static House instance;
+	public static FPWorld world;
+
+	public HouseOutline houseOutline;
 
 	public House ()
 	{
-		AddChild(houseOutline = new FSprite("HouseOutline"));
+		instance = this;
+
+		world = FPWorld.Create(80.0f);
+
+		houseOutline = HouseOutline.Create();
+		AddChild(houseOutline.holder);
+
 
 	}
 
 	public void Destroy ()
 	{
+		instance = null;
 
+		UnityEngine.Object.Destroy(world.gameObject);
+		world = null;
+	}
+}
+
+public class HouseOutline : MonoBehaviour
+{
+	public FContainer holder;
+	public FPNodeLink link;
+
+	public static HouseOutline Create()
+	{
+		var go = new GameObject();
+		go.transform.parent = House.world.transform;
+		var ho = go.AddComponent<HouseOutline>();
+
+		ho.holder = new FContainer();
+		ho.holder.AddChild(new FSprite("HouseOutline"));
+
+		ho.link = go.AddComponent<FPNodeLink>();
+		ho.link.Init (ho.holder,false);
+
+		return ho;
 	}
 }
 
